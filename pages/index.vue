4731a0cea211ebe903b1777ece6db297ac8aef53
ManhCuong <cuongdm2k1@gmail.com>
2021-03-26 17:09:27 +0700

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,82 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        myApp
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <header>
+      <div class="top-bar">
+        <header class="container mx-auto">
+          <div class="">
+            <ul class="flex">
+              <li class="p-2 text-xs">Đồ chơi</li>
+              <li class="p-2 text-xs">Hoa giấy</li>
+              <li class="p-2 text-xs">Tái chế</li>
+              <li class="p-2 text-xs">Trang trí</li>
+            </ul>
+          </div>
+        </header>
       </div>
-    </div>
+      <div class="main-menu">
+        <div class="lg:container mx-auto">
+          <div class="logo">
+            <img
+              src="~/assets/img/logo-cachlamhay-final-tiny.png"
+              alt=""
+              class="w-96 mx-auto p-4"
+            />
+          </div>
+          <div class="menu">
+            <ul class="flex justify-center">
+              <li class="text-black font-bold px-4 text-sm py-3">TRANG TRÍ</li>
+              <li class="text-black font-bold px-4 text-sm py-3">ĐỒ CHƠI</li>
+              <li class="text-black font-bold px-4 text-sm py-3">HOA GIẤY</li>
+              <li class="text-black font-bold px-4 text-sm py-3">TÁI CHẾ</li>
+              <li class="text-black font-bold px-4 text-sm py-3">COVID-19</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main class="container mx-auto">
+      <section class="flex">
+        <div class="w-1/3 relative">
+          <div class="relative">
+            <a href="">
+              <img src="~/assets/img/khay-dung-but-356x220.jpg" alt="" />
+            </a>
+          </div>
+          <div class="w-5/6 absolute text-white top-2/3">
+            <h3 class="text-xl"><a href="#">Gợi ý một số cách làm đồ handmade tái chế đơn giản, dễ dàng</a></h3>
+            <div class="text-xs">
+              <span class="font-bold">Cách làm hay</span> - <span>24/3/2021</span>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/3 relative">
+          <div class="relative">
+            <a href="">
+              <img src="~/assets/img/dreamcatcher-356x220.jpg" alt="" />
+            </a>
+          </div>
+          <div class="w-5/6 absolute text-white top-2/3">
+            <h3 class="text-xl"><a href="#">Bật mí những món đồ handmade dễ làm, bạn không nên bỏ qua</a></h3>
+            <div class="text-xs">
+                <span class="font-bold">Cách làm hay</span> - <span>24/3/2021</span>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/3 relative">
+          <div class="relative">
+            <a href="">
+              <img src="~/assets/img/lo-hoa-bang-giay-bao-cu-356x220.jpg" alt="" />
+            </a>
+          </div>
+          <div class="w-5/6 absolute text-white top-2/3">
+            <h3 class="text-xl"><a href="#">Hướng dẫn cách làm một số món đồ handmade tái chế từ giấy</a></h3>
+            <div class="text-xs">
+                <span class="font-bold">Cách làm hay</span> - <span>24/3/2021</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -32,47 +85,13 @@ export default {}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+.top-bar {
+  border-bottom: 1px solid rgb(226, 224, 224);
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.main-menu {
+  box-shadow: 0px 25px 20px -32px grey;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.container{
+  max-width: 1204px !important;
 }
 </style>
